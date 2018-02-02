e3d91cc949f15d3ca895ffdd9e5b39ca5d2ac32b
sapsolman <alexey.ozerov@me.com>
2018-02-02 23:37:02 +0300

    Update

--- 1.12.3.py
+++ 1.12.3.py
@@ -1,1 +1,9 @@
-print('Hello')
+first = int(input())
+second = int(input())
+oper = str(input())
+#answer = str
+if second == 0 and oper == '/':
+    print('Деление на 0! повторите ввод')
+else:
+    answer = first, oper, second
+    print(answer)
