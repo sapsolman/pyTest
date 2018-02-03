f986aceecf350f62858e093ee3b087d4a8d4ecdd
sapsolman <alexey.ozerov@me.com>
2018-02-03 16:25:36 +0300

    New code

--- 11.3.py
+++ 11.3.py
@@ -1,8 +1,8 @@
 first = float(input())
 second = float(input())
-oper = input()
+oper = str(input())
 if second == 0 and oper == '/':
     print('Деление на 0!')
 else:
-    answer = (first, oper, second)
+    answer = first +, oper, + second
     print(answer)
