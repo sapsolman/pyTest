b04ece684a8a2a15f24686e5bfeb932fcfee2f81
sapsolman <alexey.ozerov@me.com>
2018-02-03 13:05:33 +0300

    New code

--- 11.3.py
+++ 11.3.py
@@ -1,1 +1,8 @@
-print('Hello')
+first = float(input())
+second = float(input())
+oper = input()
+if second == 0 and oper == '/':
+    print('Деление на 0!')
+else:
+    answer = (first, oper, second)
+    print(answer)
