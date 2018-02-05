6ddc79dbeedfe3422ab2d6b924842aca2191abf8
sapsolman <alexey.ozerov@me.com>
2018-02-05 23:08:19 +0300

    Update

--- 11.3.py
+++ 11.3.py
@@ -1,13 +1,22 @@
-first = float(input())
-second = float(input())
-oper = input("Знак (+,-,*,/): ")
-if second == 0 and oper == '/':
-    print('Деление на 0!')
-elif oper == '+':
-    print(first + second)
-elif oper == '-':
-    print(first - second)
-elif oper == '*':
-    print(first * second)
-elif oper == '/':
-    print(first / second)
+first = float(input('Введите первое число '))
+second = float(input('Введите второе число '))
+oper = input('Действие (+,-,*,/,**,//,%): ')
+if oper != '+' or oper != '-' or oper != '*' or oper != '/' or oper != '**' or oper != '//' or oper != '%':
+    print('Введите действие')
+else:
+    if second == 0 and oper == '/' or oper == '%' or oper == '//':
+        print('Деление на 0!')
+    elif oper == '+':
+        print(first + second)
+    elif oper == '-':
+        print(first - second)
+    elif oper == '*':
+        print(first * second)
+    elif oper == '/':
+        print(first / second)
+    elif oper == '%':
+        print(first % second)
+    elif oper == '//':
+        print(first // second)
+    elif oper == '**':
+        print(first ** second)
