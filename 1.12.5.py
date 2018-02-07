3028c3a2bfd9f838184dd7b00da21313db8ab448
sapsolman <xlF9QTyRnOc9>
2018-02-07 23:20:30 +0300

    Программа 1.12.5 (не правильный черновик)

--- 1.12.5.py
+++ 1.12.5.py
@@ -3,13 +3,17 @@ B = int(input())
 C = int(input())
 if A >= B and B >= C:
     print(A)
-    print(B)
     print(C)
+    print(B)
 elif A >= B and A <= C:
+    print(C)
+    print(B)
+    print(A)
+elif A <= B and A <= C:
     print(C)
     print(A)
     print(B)
 else:
     print(B)
-    print(C)
     print(A)
+    print(C)
