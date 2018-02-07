cf4aac2426e7f02c680b207ef0a55e3bf321f856
sapsolman <xlF9QTyRnOc9>
2018-02-07 22:01:53 +0300

    Программа 1.12.4

--- 1.12.4.py
+++ 1.12.4.py
@@ -1,1 +1,17 @@
-#new program
+type = str(input())
+if type == 'треугольник':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    p = ((a + b + c) / 2)
+    cr = (p * (p - a) * (p - b) * (p - c))
+    print(cr ** 0.5)
+elif type == 'круг':
+    a = int(input())
+    print(3.14 * pow(a, 2))
+elif type == 'прямоугольник':
+    a = int(input())
+    b = int(input())
+    print(a * b)
+else:
+    print('Введите допустимое действие')
